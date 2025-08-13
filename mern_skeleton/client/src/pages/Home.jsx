f4ff7a60dff7eac185f64514043c7355cb20964f
RichardJiang1024 <ij1631861@gmail.com>
2025-08-13 11:50:47 -0400

    Change img to image

--- mern_skeleton/client/src/pages/Home.jsx
+++ mern_skeleton/client/src/pages/Home.jsx
@@ -22,7 +22,7 @@ function Home({ user }) {
 
   return (
     <div style={{ padding: '60px 20px', textAlign: 'center' }}>
-      <img src="/images/logo.png" alt="Team Logo" style={{ width: '120px', marginBottom: '20px' }} />
+      <image src="/images/logo.png" alt="Team Logo" style={{ width: '120px', marginBottom: '20px' }} />
       <h1>Welcome to Rapid Tasks</h1>
       <p>Manage your tasks quickly and efficiently with our team-built app.</p>
 
